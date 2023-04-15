be70c0bcb904fefb9089377f2dbf06ef307c41cf
ade guntur <ade.guntur@bnisekuritas.co.id>
2023-04-15 07:49:27 +0700

    first commit

---
+++ inc/config/constants.php
@@ -0,0 +1,21 @@
+<?php
+	// Root url for the site
+	define('ROOT_URL', 'http://localhost/inventory/');
+	
+	
+	// Database parameters
+	// Data source name
+	define('DSN', 'mysql:host=localhost:3306;dbname=shop_inventory');
+	
+	// Hostname
+	define('DB_HOST', 'localhost:3306');
+	
+	// DB user
+	define('DB_USER', 'root');
+	
+	// DB password
+	define('DB_PASSWORD', 'password');
+	
+	// DB name
+	define('DB_NAME', 'shop_inventory');
+?>
